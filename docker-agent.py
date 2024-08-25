e098be04b12bac1bd09b3ced6d850f2402065d74
Robin Kavenius <robin.kavenius@yh.nackademin.se>
2024-08-25 16:11:57 +0200

    feat: narrow container stats return
    Agent now only returns container name, cpu/mem percent,
    and status in flask response.

--- docker-agent.py
+++ docker-agent.py
@@ -1,57 +1,83 @@
 #!/usr/bin/env python3
 
-# Make a curl request for testing:
-# curl -x <method> <url> OPTIONAL: -H <header> -d <POST data>
+# TODO - Stream stats instead of snapshot.
+# TODO - Error handling - crashing if a container is stopped
+# TODO - SLOW! Gathering stats is slow even with only two containers.
+# TODO - Container status not updating on GET request?
+
+# curl -x <method> <url> -d <POST data>
 
 import docker
 from flask import Flask, request, jsonify
 
-# Create a Flask app
 app = Flask(__name__)
 
-# Create a client for the local docker engine
 client = docker.from_env()
 
-# THOUGHT - This only fetches containers at start, how to update on new containers without agent reboot?
-# Creates a list of all container names.
-ct_names = [x.name for x in client.containers.list(all=True)]
-print(ct_names)
+# This only fetches containers at start, how to update on new containers without agent reboot?
+# Creates a dict with <ct_name>: <ct_object> key-value pairs.
+containers = {x.name: x for x in client.containers.list(all=True)}
 
 
 # API Info?
 @app.route("/stats", methods=["GET"])
-def api_stats():
+def client_api_stats():
+    """
+    API endpoint for getting docker api stats
+    """
     items = client.api.info()
     return jsonify(items)
 
 
-# THOUGHT - Getting all information is time consuming in a single thread, multithread?
+# Getting all information is time consuming in a single thread, multithreading?
 @app.route("/container/all", methods=["GET"])
 def ct_stats_all():
-    ct_stats_list = [
-        client.containers.get(name).stats(stream=False) for name in ct_names
-    ]
-    return jsonify(ct_stats_list)
+    """
+    API endpoint for getting stats from all containers.
+    """
+
+    # Get hostname
+    hostname = client.api.info().get("Name")
+
+    # Create nested dict with hostname as top value
+    all_stats = dict()
+    all_stats[hostname] = dict()
+
+    # Add container stats from global containers dict.
+    all_stats[hostname].update(
+        {
+            key: value
+            for container in containers.values()
+            for key, value in pick_stats(container).items()
+        }
+    )
+
+    return jsonify(all_stats)
 
 
 @app.route("/container/<name>", methods=["GET"])
 def ct_stats(name):
+    """
+    API endpoint for fetching container stats.
+    """
     # Look if the name exists and create it's container object.
-    if name in ct_names:
-        cont = client.containers.get(name)
+    if name in containers.keys():
         # Return a single snapshot of that containers stats, converted to json.
-        return jsonify(cont.stats(stream=False))
+        return jsonify(pick_stats(containers[name]))
     else:
         return not_found(name)
 
 
-# THOUGHT - Change this to a POST
+# Change this to a POST
 @app.route("/container/<name>/stop", methods=["GET"])
 def ct_stop(name):
+    """
+    Stop a container by name
+    """
     # Look if the name exists and create it's container object.
-    if name in ct_names:
+    if name in containers.keys():
         cont = client.containers.get(name)
-        # Try to stop the container, return an error if not successful (APIerror raised).
+        # Try to start the container, return an error if not successful (APIerror raised).
         try:
             cont.stop()
             return f"Stopped {name}.", 200
@@ -61,11 +87,14 @@ def ct_stop(name):
         return not_found(name)
 
 
-# THOUGHT - Change this to a POST
+# Change this to a POST
 @app.route("/container/<name>/start", methods=["GET"])
 def ct_start(name):
+    """
+    Start a container by name
+    """
     # Look if the name exists and create it's container object.
-    if name in ct_names:
+    if name in containers.keys():
         cont = client.containers.get(name)
         # Try to start the container, return an error if not successful (APIerror raised).
         try:
@@ -77,11 +106,64 @@ def ct_start(name):
         return not_found(name)
 
 
-# If the container doesnt exist, return a message and 404.
 def not_found(name):
+    """
+    Return a not found message and a 404 status code.
+    """
     return f"{name} not found.", 404
+
+
+def pick_stats(ct):
+    """
+    Picks out relevant container stats and formats them in a nested dict.
+    """
+
+    # Save container stats
+    stats = ct.stats(stream=False)
+
+    # Nested dict with container name as the top.
+    ct_stats = dict()
+    ct_stats[ct.name] = dict()
+
+    # Status
+    ct_stats[ct.name]["status"] = ct.status
+
+    # _________________________________
+    # Format CPU usage as a percentage.
+    cpu_stats = stats["cpu_stats"]
+    precpu_stats = stats["precpu_stats"]
+
+    # Container CPU
+    ct_cpu = (
+        cpu_stats["cpu_usage"]["total_usage"] - precpu_stats["cpu_usage"]["total_usage"]
+    )
+    # System CPU
+    system_cpu = cpu_stats["system_cpu_usage"] - precpu_stats["system_cpu_usage"]
+    # Number of cores
+    cores = cpu_stats["online_cpus"]
+
+    # Container CPU / System CPU * Number of cores * 100
+    ct_stats[ct.name]["cpu_percent"] = round(ct_cpu / system_cpu * cores * 100, 2)
+
+    # ___________________________________
+    # Format memory usage as a percentage.
+    mem_stats = stats["memory_stats"]
+
+    # Memory usage / Memory limit * 100
+    # Docker stats shows 0.01%, this shows 0.03%...hmmmm
+    ct_stats[ct.name]["mem_percent"] = round(
+        mem_stats["usage"] / mem_stats["limit"] * 100, 2
+    )
+
+    return ct_stats
 
 
 if __name__ == "__main__":
     # Start the webserver listening on all interfaces.
     app.run(host="0.0.0.0", debug=True)
+
+
+# Stream stats on a 5 sec timer
+# while True:
+#   pprint.pprint(next(ct.stats(decode=True)))
+#   time.sleep(5)
